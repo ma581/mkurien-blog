66e68aa1f4273b2f7f98761bc2901220cb70a14e
maku <manojasho@gmail.com>
2021-08-11 16:15:06 +0100

    Router and npm audit fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import './App.css';
-import {NavBar} from './Navbar';
+import { NavBar } from './Navbar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <h1>Welcome to my blog!</h1>
+      <NavBar />
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/articles">
+              <Articles />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
+
+function Home() {
+  return <h1>Welcome!</h1>
+
+}
+
+function Articles() {
+  return <h2>Articles</h2>;
+}
+
+function Contact() {
+  return <h2>Contact</h2>;
+}
+
 
 export default App;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,15 @@
 import { Container, Navbar, Nav } from 'react-bootstrap';
 
 export function NavBar(props){
-return (<>
-
+return (
   <Navbar bg="primary" variant="dark">
     <Container>
-    <Navbar.Brand href="#home">MK</Navbar.Brand>
+    <Navbar.Brand href="home">MK</Navbar.Brand>
     <Nav className="me-auto">
-      <Nav.Link href="#about">About</Nav.Link>
-      <Nav.Link href="#articles">Articles</Nav.Link>
-      <Nav.Link href="#articles">Contact</Nav.Link>
+      <Nav.Link href="/articles">Articles</Nav.Link>
+      <Nav.Link href="/contact">Contact</Nav.Link>
     </Nav>
     </Container>
   </Navbar>
-</>)
+)
 }
